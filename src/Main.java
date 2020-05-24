2fd94bc2c26c08f7f5b73e39e14fe84f2dc3a6e2
Liza <bliza0812@gmail.com>
2020-05-24 18:36:25 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int counter = 0;
+        int summ = 0;
+        while(summ != 10){
+            summ+=in.nextInt();
+            counter+=1;
+        }
+        System.out.println(counter);
+        in.close();
+        }
     }
-}
